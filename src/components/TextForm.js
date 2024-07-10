fc1c2382d477e23034cf5ff597eddd8cb76b7d07
Nilesh Patil (nileshp) <nileshp@cisco.com>
2024-07-10 23:48:51 +0530

    new functionality added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,6 +9,16 @@ export default function TextForm(props) {
         let newtext =Text.toUpperCase();
         setText(newtext)
     }
+    const handlelowClick = ()=>{
+        // console.log("upper case ws clicked" + Text)
+        let newtext =Text.toLowerCase();
+        setText(newtext)
+    }
+    const handleclearClick = ()=>{
+        // console.log("upper case ws clicked" + Text)
+        let newtext ="";
+        setText(newtext)
+    }
 
     const handleOnChange = (event)=>{
         // console.log("on change ");
@@ -16,18 +26,29 @@ export default function TextForm(props) {
         
     }
 
-    const [Text , setText] = useState("Enter the text");
+    const [Text , setText] = useState("");
     
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        
-            <div className="form-group mb-3">
-                
-                <textarea className="form-control" value={Text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-            </div>
-            <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
-    </div>
+    <>
+        <div className="container">
+            <h1>{props.heading}</h1>
+            
+                <div className="form-group mb-3">
+                    
+                    <textarea className="form-control" value={Text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-primary mx-2' onClick={handlelowClick}>Convert to Lowercase</button>
+                <button className='btn btn-primary mx-2' onClick={handleclearClick}>Clear text</button>
+        </div>
+        <div className="container my-2">
+                <h1>Your Text Summary</h1>
+                <p> {Text.split(" ").length} words and {Text.length} characters</p>
+                <p>{0.008 * Text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{Text}</p>
+        </div>
+    </>
   )
 }
 
